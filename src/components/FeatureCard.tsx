8447d90ad85fc6ea03751512e8edd2cd4a18e28b
Natdanai Khemtong <git hub email>
2025-06-20 19:55:19 +0700

    [feat]  add carousel for feature movie card

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -1,9 +1,15 @@
 import type { MovieType } from "@/types";
-import { Flame, TrendingUp } from "lucide-react";
+import { ChevronLeft, ChevronRight, Flame, TrendingUp } from "lucide-react";
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
+import { Navigation, Scrollbar } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/navigation";
+import "swiper/css";
+import { useRef } from "react";
 
 function FeatureCard({ movies, type }: { movies: MovieType[]; type: string }) {
+
   if (!movies || movies.length === 0) {
     return (
       <div className="w-full h-full flex justify-center items-center">
@@ -44,19 +50,36 @@ function FeatureCard({ movies, type }: { movies: MovieType[]; type: string }) {
       </div>
 
       {/* Scrollable movie list */}
-      <div className="w-full overflow-x-auto mt-4 custom-scrollbar ">
-        <div className="flex gap-4">
-          {movies &&
-            movies.map(movie => (
-              <div
-                key={movie.id}
-                className="min-w-[calc(100%/1)] sm:min-w-[calc(100%/2)] md:min-w-[calc(100%/3)] lg:min-w-[calc(100%/4)]"
-              >
-                <MovieCard movie={movie} />
-              </div>
-            ))}
-        </div>
-      </div>
+      <Swiper
+        modules={[Navigation, Scrollbar]}
+        spaceBetween={50}
+        slidesPerView={4}
+        breakpoints={{
+          1440:{
+            slidesPerView: 4,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          320: {
+            slidesPerView: 1,
+          },
+        }}
+        onReachEnd={() => {}}
+        navigation
+        scrollbar={{ draggable: true }}
+        className="w-full flex flex-row overflow-x-auto mt-4"
+      >
+        {movies &&
+          movies.map(movie => (
+            <SwiperSlide>
+              <MovieCard key={movie.id} movie={movie} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
     </div>
   );
 }
